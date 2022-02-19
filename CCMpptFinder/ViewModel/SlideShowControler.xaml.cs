6538c5098e0d6969c51beb3d2a953f2f770e4691
patternplane <bss011202@naver.com>
2022-02-19 14:29:20 +0900

    [ADD] SlideShow Controler 생성 및 레이아웃 잡기
    SlideShow Controler 윈도우를 생성하였다.

--- CCMpptFinder/ViewModel/SlideShowControler.xaml.cs
+++ CCMpptFinder/ViewModel/SlideShowControler.xaml.cs
@@ -10,27 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CCMpptFinder
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// SlideshowControler.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SlideShowControler : Window
     {
-        public MainWindow()
+        public SlideShowControler()
         {
-            //Search.test();
-            //Powerpoint pptClass = new Powerpoint();
-            //pptClass.test();
-
-
             InitializeComponent();
-
-            SlideShowControler showview = new SlideShowControler();
-            showview.Show();
         }
     }
 }
