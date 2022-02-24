4bdce05ae891d2c7a36a9aeece075cfca34ba9c6
patternplane <bss011202@naver.com>
2022-02-24 21:10:01 +0900

    [FEAT] 슬라이드쇼 레이아웃 기초 완성
    미리보기 창과 선택 창이 완성되었음

--- CCMpptFinder/ViewModel/SlideShowControler.xaml.cs
+++ CCMpptFinder/ViewModel/SlideShowControler.xaml.cs
@@ -22,13 +22,39 @@ namespace CCMpptFinder
         public SlideShowControler()
         {
             InitializeComponent();
-
+            
+            previewImageFrame.DataContext = new slidePreviewImage() { previewImage = "C:\\Users\\Sun\\Desktop\\프레젠테이션1\\슬라이드1.jpg" };
 
             TestList.Items.Add(new slideObject() { number = "number", source = "C:\\Users\\Sun\\Desktop\\프레젠테이션1\\슬라이드1.jpg" }) ;
             TestList.Items.Add(new slideObject() { number = "Apricot2", source = "C:\\Users\\Sun\\Desktop\\프레젠테이션1\\제목 없음.png" });
             TestList.Items.Add(new slideObject() { number = "Apricot3", source = "C:\\Users\\Sun\\Desktop\\프레젠테이션1\\제목 없음.png" });
             TestList.Items.Add(new slideObject() { number = "Apricot4", source = "C:\\Users\\Sun\\Desktop\\프레젠테이션1\\슬라이드1.jpg" });
         }
+
+        // 스크롤바 마우스 휠 동작
+        // 출처:
+        // https://onlab94.tistory.com/116 [ㅇㅅㅎ]
+        // https://stackoverflow.com/questions/43053345/wpf-accessing-scrollviewer-of-a-listview-codebehind
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Decorator border = VisualTreeHelper.GetChild((ListView)sender, 0) as Decorator;
+            ScrollViewer sv = border.Child as ScrollViewer;
+            if (0 > e.Delta)
+            {
+                sv.LineRight();
+            }
+            else
+            {
+                sv.LineLeft();
+            }
+            e.Handled = true;
+        }
+
+    }
+
+    class slidePreviewImage
+    {
+        public String previewImage { set; get; }
     }
 
     class slideObject
