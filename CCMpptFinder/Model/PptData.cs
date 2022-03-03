9dfc1ecee3d68b6ed22da0600d16f52a0007de09
patternplane <bss011202@naver.com>
2022-03-03 18:24:30 +0900

    [ADD] PptData 클래스가 추가됨
    각 가사ppt별 정보를 관리합니다.

---
+++ CCMpptFinder/Model/PptData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// file 처리를 위해 추가 (Directory Class)
+using System.IO;
+
+namespace CCMpptFinder
+{
+    class PptData
+    {
+        public PptData()
+        {
+            
+        }
+    }
+}
