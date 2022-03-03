182a2646dc7b5d81669e87197c85d0ef8e82418b
patternplane <bss011202@naver.com>
2022-03-03 17:56:12 +0900

    [FEAT] File 클래스의 이름 변경 및 파일확인 기능 추가
    File 클래스의 이름이 FileManager로 변경되었습니다.
    특정 파일(혹은 경로)이 존재하는지 점검하는 CheckDataFiles 메소드가 추가되었습니다.
    프로그램의 경로를 반환하는 getCurrentDir 메소드가 추가되었습니다.

---
+++ CCMpptFinder/Model/FileManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace CCMpptFinder
+{
+    class FileManager
+    {
+        // 특정 디렉토리의 ppt 프레젠테이션 파일 목록을 생성합니다.
+        static public String[] getPptFiles(String path) 
+        {
+            try
+            {
+                String[] files = Directory.GetFiles(path,"*.ppt?");
+                return files;
+            }
+            catch 
+            {
+                return null;
+            }
+
+        }
+
+        static public String getCurrentDir()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        /// <summary>
+        /// 요청한 데이터파일이 해당 경로에 존재하는지 검사합니다.<br/>
+        /// 만약 없으면 해당 파일을 만들어둡니다.
+        /// </summary>
+        /// <param name="paths">
+        /// 검사할 데이터파일의 현재 프로그램으로부터의 상대 경로를 적습니다.
+        /// </param>
+        /// <returns>에러의 횟수를 반환합니다.</returns>
+        static public int CheckDataFiles(String[] paths)
+        {
+            int error = 0;
+            foreach(String path in paths)
+            {
+                try
+                {
+                    if (!Directory.Exists(getCurrentDir() + "\\" + Path.GetDirectoryName(path)))
+                        Directory.CreateDirectory(getCurrentDir() + "\\" + Path.GetDirectoryName(path));
+                    if (!File.Exists(getCurrentDir() + "\\" + path))
+                        File.Create(getCurrentDir() + "\\" + path);
+                }
+                catch (Exception e)
+                {
+                    error++;
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return error;
+        }
+
+        
+    }
+}
